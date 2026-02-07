d1069e6711b236e303ec6fc7da6d89bac622fab6
Harihara369 <hariharapidikiti@gmail.com>
2026-02-07 12:50:59 +0530

    first commit

---
+++ day1/TypeCasting.java
@@ -0,0 +1,18 @@
+package com.version1;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+		
+    	int a =10;
+    	
+    	double b = a;
+    	
+    	System.out.println(b);
+    	
+    	double c = 8.934525455643;
+    	
+    	short d = (short)c;
+    	
+    	System.out.println(d);
+	}
+}
